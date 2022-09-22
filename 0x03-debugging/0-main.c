12de3afbd6e7a4f413d19df1da911e5c4e79d5f6
ChibuzorBiodun <chibuzorebo@gmail.com>
2022-09-22 07:10:09 -0700

    This project is about debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests that the function that prints if integer is positive gives a correct output when given a case of 0
+ * Return: 0
+ * /
+ 
+ int main(void)
+ {
+ 	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
+
